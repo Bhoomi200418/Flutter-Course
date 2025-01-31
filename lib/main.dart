6cfac18d672a4773e3f2627985c7c484bf39919d
bhoomi200418 <bhoomi2004patel@gmail.com>
2025-01-31 10:41:22 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'package:adv_basics/quiz.dart';
+
+void main() {
+  
+  runApp(const Quiz());
+}
+  // MaterialApp(
+  //   home: Scaffold(
+  //     body: Container(
+  //       decoration: const BoxDecoration(
+  //       gradient: LinearGradient(
+  //         colors: [
+  //            Color.fromARGB(255, 197, 177, 232),
+  //            Color.fromARGB(255, 133, 78, 227),
+  //         ],
+  //         begin: Alignment.topLeft,
+  //         end: Alignment.bottomRight,
+  //         ),
+  //       ),
+  //       child: const StartScreen(),
+  //       ),
+  //     ),
+  //   );
+// }
---
+++ lib/questions_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
+
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    setState(() {
+      currentQuestionIndex++; // increments the valur by 1
+    });
+  }
+
+  @override
+  Widget build(context) {
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 248, 123, 123),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+
+            ...currentQuestion.shuffledAnswers.map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {
+                  answerQuestion(answer);
+                },
+              );
+            }),
+            // AnswerButton(
+            //   answerText: currentQuestion.answer[0],
+            //   onTap: () {},
+            // ),
+            // AnswerButton(
+            //   answerText: currentQuestion.answer[1],
+            //   onTap: () {},
+            // ),
+            // AnswerButton(
+            //   answerText: currentQuestion.answer[2],
+            //   onTap: () {},
+            // ),
+            // AnswerButton(
+            //   answerText: currentQuestion.answer[3],
+            //   onTap: () {},
+            // ),
+
+            // ElevatedButton(
+            //   onPressed: () {},
+            //   style: ElevatedButton.styleFrom(),
+            //    child: const Text('Answer 1'),
+            // ),
+            //  ElevatedButton(
+            //   onPressed: () {},
+            //    child: const Text('Answer 2'),
+            // ),
+            //  ElevatedButton(
+            //   onPressed: () {},
+            //    child: const Text('Answer 3'),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
